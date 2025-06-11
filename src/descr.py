fb743b16f947ec233b5c8c93e40071bb96de1661
DE3PM <de3pm.exclusives@gmail.com>
2025-06-11 19:43:12 +0530

    Update descr.py

--- src/descr.py
+++ src/descr.py
@@ -63,4 +63,4 @@ class Description:
 
         with open(self.description_bbcode_path, "w", encoding="utf-8") as f:
             f.write(new_content)
-            f.write('[b][center][url=https://github.com/xDE3PM/BWT-Uploader][color=RoyalBlue]Created by BWT-Uploader[/color][/url][/center][/b]\n')
+            f.write('[b][center][url=https://github.com/xDE3PM/BWT-Uploader][color=red]Created by BWT-Uploader[/color][/url][/center][/b]\n')
